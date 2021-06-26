ce087508779142e5c942e1ada480250bd38d476b
BANO.notIT <bano.notit@yandex.ru>
2021-06-26 21:55:54 +0300

    feat(cli): use __main__ with typer

--- toggl2rm/__main__.py
+++ toggl2rm/__main__.py
@@ -1,13 +1,6 @@
-import pandas as pd
-from extract.toggl import extract_time
-from extract.redmine import extract_issues
-from transform import rename_description_to_comments, get_issue_id_from_comments
+from __future__ import absolute_import
 
-df = rename_description_to_comments(extract_time(workspace=4_513_131, projects=166_567_787))
-df = get_issue_id_from_comments(df)
-print(pd.isnull(df['issue_id']))
-print(df)
+from .cli.main import app
 
-df = extract_issues('https://rm.onlystudio.org/', project=11, assigned_to='me')
-print(df.dtypes)
-print(df)
+if __name__ == '__main__':
+    app()
