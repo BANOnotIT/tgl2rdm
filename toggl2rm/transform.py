c0e163293112ace999f192796083822ac6e9cc4a
BANO.notIT <bano.notit@yandex.ru>
2021-06-26 21:53:35 +0300

    feat(transform): named objects and drain issues selector

--- toggl2rm/transform.py
+++ toggl2rm/transform.py
@@ -1,6 +1,6 @@
 import re
 from datetime import timedelta
-from typing import Tuple
+from typing import Tuple, List
 
 import pandas as pd
 import petl as etl
@@ -19,7 +19,7 @@ def get_issue_id_from_comments(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def parse_datetime(inp, date_fields: Tuple[str]):
+def parse_datetime(inp, date_fields: List[str]):
     return etl.convert(inp, date_fields, parsedatetime)
 
 
@@ -36,3 +36,30 @@ def add_issue_id_from_description(inp):
             return None
 
     return etl.addfield(inp, 'issue_id', m)
+
+
+def extract_named_objects_to_columns(inp, named_object_columns: List[str]):
+    res = inp
+
+    def m(column1: str, prop1: str):
+        def mod(row):
+            if row.get(column1):
+                return row[column1].get(prop1, None)
+            return None
+
+        return mod
+
+    for column in named_object_columns:
+        for prop in ('id', 'name'):
+            res = etl.addfield(res, f'{column}_{prop}', m(column, prop))
+
+    return res
+
+
+def select_drain_issues(inp, assignee_id: int, drain_cf_id: int):
+    def is_drain(fields: list) -> bool:
+        return any(map(lambda field: field['id'] == drain_cf_id and field['value'] == '1', fields))
+
+    # custom fields have more selectivity
+    inp = etl.select(inp, 'custom_fields', is_drain)
+    return etl.selecteq(inp, 'assigned_to_id', assignee_id)
