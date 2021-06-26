a4f6c9374ae5502ba90d212313da9e6a7f2ae839
BANO.notIT <bano.notit@yandex.ru>
2021-06-26 21:50:43 +0300

    feat(cli): example config and schema

---
+++ toggl2rm/cli/config_parser.py
@@ -0,0 +1,31 @@
+from schema import Schema, And, Or, Optional
+
+config_simple_schema = Schema({
+    'redmine': {
+        'token': str,
+        'url': str,
+    },
+    'toggl': {
+        'token': str,
+        'workspace_id': int,
+    },
+    Optional('default_attrs'): {
+        Optional('sprint'): str,
+        Optional('rdm_activity_id'): int,
+    },
+    'project': {
+        str: {
+            'rdm_project_id': Or(str, int),
+            'tgl_project_id': int,
+            Optional('sprint'): str,
+            Optional('rdm_activity_id'): int,
+        }
+    }
+})
+
+
+def validate_config(config: str):
+    validated = config_simple_schema.validate(config)
+    # todo: add access checks
+    # todo: add project validations check
+    return True
